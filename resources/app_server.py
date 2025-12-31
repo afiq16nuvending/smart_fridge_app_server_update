cfe0000c958122cabc93ad304aeb61219db84c64
afiq16nuvending <afiq16.nuvending@gmail.com>
2025-12-31 10:18:07 +0800

    Comment out product names in app_server.py
    Comment out product names in class_names mapping.

--- resources/app_server.py
+++ resources/app_server.py
@@ -436,15 +436,15 @@ def draw_counts(frame, class_counters, label):
     # Product name mapping (class ID to product name)
     class_names = {
     0: "",
-    1: "100plus",
+    #1: "100plus",
     2: "chickenKatsuCurry",
-    3: "cocacola",
-    4: "coconut", 
+    #3: "cocacola",
+    #4: "coconut", 
     5: "dakgangjeongRice",
     6: "kimchiFriedRice", 
     7: "kimchiTuna",
-    8: "lemon", 
-    9: "orange", 
+    #8: "lemon", 
+    #9: "orange", 
       
 }
     
